78bd10b2b13bc18e392ca56c5608156863a68713
Eliza Keating <ek.elizakeating@gmail.com>
2023-05-31 13:31:11 -0600

    Reassign string variables and use put statements

--- variables.rb
+++ variables.rb
@@ -9,3 +9,9 @@ second_float = 7.99
 tired = true
 excited = true
 hungry = false
+first_name = "Tim"
+favorite_color = "Green"
+favorite_animal = "Dog"
+puts "My name is #{first_name}"
+puts "My favorite animal is #{favorite_animal}"
+puts "I have $#{price}"
